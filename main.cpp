098d4482d1fa7b05f86550fe9be39e68ce20619a
TyrionBian <tianliang.bian@gmail.com>
2017-09-29 02:11:18 +0800

    add play tree ,not complete

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 
 //#include "huffman_encoding/huffman_code.h"
 //#include "huffman_encoding/huffman_code_sec.hpp"
-#include "AVL_tree/avl_tree.c"
+//#include "AVL_tree/avl_tree.c"
+#define node splay_node
+#define rightRotate splay_node
+#include "splay_tree/splay_tree.c"
+#undef node
 int main() {
 
     std::cout << "Hello, World!" << std::endl;
@@ -14,7 +18,7 @@ int main() {
     using namespace Cpp;
     HuffmanCodes_Cpp(arr, freq, size);*/
     // test avl tree
-    struct Node *root = NULL;
+/*    struct Node *root = NULL;
     root = insert(root, 10);
     root = insert(root, 20);
     root = insert(root, 30);
@@ -26,6 +30,17 @@ int main() {
     preOrder(root);
     root = deleteNode(root, 10);
     printf("\nPreorder traversal after deletion of 10 \n");
+    preOrder(root);*/
+    // test splay tree
+    struct splay_node *root = newNode(100);
+    root->left = newNode(50);
+    root->right = newNode(200);
+    root->left->left = newNode(40);
+    root->left->left->left = newNode(30);
+    root->left->left->left->left = newNode(20);
+
+    root = search(root, 20);
+    printf("Preorder traversal of the modified Splay tree is \n");
     preOrder(root);
     return 0;
 }
